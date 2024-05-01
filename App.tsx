4bce3a229d85a055866be0c8ecb1e149e138f3e1
rajesh <sangapogurajesh@gmail.com>
2024-05-01 23:17:32 +0530

    small

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import Splash from './src/screens/Splash/Splash'
-import Login from './src/screens/Login/Login'
 import EntryStack from './src/navigation/EntryStack'
 
 export default function App() {
