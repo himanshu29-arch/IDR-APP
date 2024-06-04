1c7629c3aa681fdd84618211ef5bc43f13381909
Rajkamal Singh <rajkamalsinghrn@gmail.com>
2024-06-04 11:50:35 +0530

    add hitslop to increase clickable are

--- src/components/customDropdown.tsx
+++ src/components/customDropdown.tsx
@@ -1,16 +1,25 @@
-import React, { Dispatch, SetStateAction, useState } from 'react';
-import { View, TouchableOpacity, StyleSheet, Image, Pressable, ScrollView, FlatList, Modal } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import { ImageStyle, StyleProp, TextStyle, ViewStyle } from 'react-native';
-import MyText from './customtext';
-import { AppColors } from '../utils/colors';
-import { SCREEN_HEIGHT, SCREEN_WIDTH } from '../utils/Dimensions';
+import React, { Dispatch, SetStateAction, useState } from "react";
+import {
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  Pressable,
+  ScrollView,
+  FlatList,
+  Modal,
+} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { ImageStyle, StyleProp, TextStyle, ViewStyle } from "react-native";
+import MyText from "./customtext";
+import { AppColors } from "../utils/colors";
+import { SCREEN_HEIGHT, SCREEN_WIDTH } from "../utils/Dimensions";
 
 type styleprops = {
   ViewStyles: ViewStyle;
   TextStyle: TextStyle;
-  ImageStyle: ImageStyle
-}
+  ImageStyle: ImageStyle;
+};
 type props = {
   options: string[];
   onSelect: Dispatch<SetStateAction<string>>;
@@ -20,8 +29,16 @@ type props = {
   isDisabled?: boolean;
   isDarker?: boolean;
   type?: string;
-}
-const CustomDropdown = ({ options, onSelect, defaultOption, isDarker, label, isDisabled, type }: props) => {
+};
+const CustomDropdown = ({
+  options,
+  onSelect,
+  defaultOption,
+  isDarker,
+  label,
+  isDisabled,
+  type,
+}: props) => {
   const [isVisible, setIsVisible] = useState(false);
   const [selectedOption, setSelectedOption] = useState("");
 
@@ -36,37 +53,49 @@ const CustomDropdown = ({ options, onSelect, defaultOption, isDarker, label, isD
     onSelect(option);
     setIsVisible(false);
   };
-const getVal = () => {
-  if(type === "client" && selectedOption !== "" ){
-   return selectedOption.company_name
-  } else if(type === "location" && selectedOption !== ""){
-    return selectedOption.address_line_one
-   } if(type === "status" && selectedOption !== ""){
-    return selectedOption
-   } else if(defaultOption !== ""){
-    return defaultOption
-   } else{
-    return "Choose Option"
-   }
-}
+  const getVal = () => {
+    if (type === "client" && selectedOption !== "") {
+      return selectedOption.company_name;
+    } else if (type === "location" && selectedOption !== "") {
+      return selectedOption.address_line_one;
+    }
+    if (type === "status" && selectedOption !== "") {
+      return selectedOption;
+    } else if (defaultOption !== "") {
+      return defaultOption;
+    } else {
+      return "Choose Option";
+    }
+  };
   return (
-    <View style={{marginBottom: 5}}>
-      {label && (
-       <MyText style={{  color: AppColors.black }}>{label}</MyText>
-      )}
+    <View style={{ marginBottom: 5 }}>
+      {label && <MyText style={{ color: AppColors.black }}>{label}</MyText>}
       <View>
-       {
-        isDisabled ?
-        <View style={isDarker ? styles.anotherC : styles.mainc}>
-        <MyText fontType="medium" style={isDarker ? styles.anothertxt : styles.maintxt}>{getVal()}</MyText>
-        <Ionicons size={25} color={AppColors.black} name="chevron-down" />
-      </View>
-      :
-      <TouchableOpacity style={isDarker ? styles.anotherC : styles.mainc} onPress={toggleDropdown}>
-      <MyText fontType="medium" style={isDarker ? styles.anothertxt : styles.maintxt}>{getVal()}</MyText>
-      <Ionicons size={25} color={AppColors.black} name="chevron-down" />
-    </TouchableOpacity>
-       }
+        {isDisabled ? (
+          <View style={isDarker ? styles.anotherC : styles.mainc}>
+            <MyText
+              fontType="medium"
+              style={isDarker ? styles.anothertxt : styles.maintxt}
+            >
+              {getVal()}
+            </MyText>
+            <Ionicons size={25} color={AppColors.black} name="chevron-down" />
+          </View>
+        ) : (
+          <TouchableOpacity
+            style={isDarker ? styles.anotherC : styles.mainc}
+            onPress={toggleDropdown}
+            hitSlop={12}
+          >
+            <MyText
+              fontType="medium"
+              style={isDarker ? styles.anothertxt : styles.maintxt}
+            >
+              {getVal()}
+            </MyText>
+            <Ionicons size={25} color={AppColors.black} name="chevron-down" />
+          </TouchableOpacity>
+        )}
         <Modal
           animationType="fade"
           transparent={true}
@@ -74,34 +103,37 @@ const getVal = () => {
           onRequestClose={() => setIsVisible(false)}
         >
           <View style={styles.container}>
-
             <View style={styles.contentContainer}>
-              <Ionicons name='close' color={AppColors.black} size={30} style={{ alignSelf: 'flex-end' }} onPress={() => setIsVisible(false)} />
-             <FlatList
-              data={options}
-              style={{maxHeight: SCREEN_HEIGHT*0.6, width: SCREEN_WIDTH*0.8}}
-              renderItem={({item, index}) =>
-                <TouchableOpacity
-              key={index}
-              style={styles.option}
-              onPress={() => handleSelectOption(item)}
-            >
-              {
-                type === "client" ?
-                  <MyText fontType="medium">
-                    {item.company_name}
-                  </MyText>
-                  : type === "location" ?
-                  <MyText fontType="medium">
-                    {item.address_line_one}
-                  </MyText>
-                  :type === "status" ?
-                  <MyText fontType="medium">
-                    {item}
-                  </MyText> :null
-              }
-            </TouchableOpacity>}
-             />
+              <Ionicons
+                name="close"
+                color={AppColors.black}
+                size={30}
+                style={{ alignSelf: "flex-end" }}
+                onPress={() => setIsVisible(false)}
+              />
+              <FlatList
+                data={options}
+                style={{
+                  maxHeight: SCREEN_HEIGHT * 0.6,
+                  width: SCREEN_WIDTH * 0.8,
+                }}
+                renderItem={({ item, index }) => (
+                  <TouchableOpacity
+                    hitSlop={12}
+                    key={index}
+                    style={styles.option}
+                    onPress={() => handleSelectOption(item)}
+                  >
+                    {type === "client" ? (
+                      <MyText fontType="medium">{item.company_name}</MyText>
+                    ) : type === "location" ? (
+                      <MyText fontType="medium">{item.address_line_one}</MyText>
+                    ) : type === "status" ? (
+                      <MyText fontType="medium">{item}</MyText>
+                    ) : null}
+                  </TouchableOpacity>
+                )}
+              />
             </View>
           </View>
         </Modal>
@@ -113,7 +145,8 @@ const getVal = () => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "rgba(0, 0, 0, 0.5)",
-    alignItems: 'center', justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     height: SCREEN_HEIGHT,
   },
   label: {
@@ -121,19 +154,19 @@ const styles = StyleSheet.create({
     marginTop: 10,
     color: AppColors.black,
   },
-  contentContainer: { 
-    backgroundColor: AppColors.white, 
-    paddingTop: 20, 
-    paddingHorizontal: 20, 
+  contentContainer: {
+    backgroundColor: AppColors.white,
+    paddingTop: 20,
+    paddingHorizontal: 20,
     borderRadius: 20,
-    marginTop: -50
+    marginTop: -50,
   },
   header: {
     padding: 10,
     borderWidth: 1,
-    borderColor: 'black',
+    borderColor: "black",
     borderRadius: 5,
-    backgroundColor: 'white',
+    backgroundColor: "white",
   },
   headerText: {
     fontSize: 16,
@@ -142,10 +175,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: AppColors.darkgreyColor,
     borderRadius: 5,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     zIndex: 1,
     width: SCREEN_WIDTH * 0.8,
-    alignSelf: 'center'
+    alignSelf: "center",
   },
   option: {
     padding: 10,
@@ -153,28 +186,28 @@ const styles = StyleSheet.create({
   mainc: {
     width: SCREEN_WIDTH * 0.8,
     padding: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     borderColor: AppColors.darkgrey,
     borderRadius: 10,
     borderWidth: 1,
-    marginTop: 5
+    marginTop: 5,
   },
   anotherC: {
     width: SCREEN_WIDTH * 0.8,
     padding: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     borderRadius: 20,
     backgroundColor: AppColors.lightgrey,
-    marginTop: 5
+    marginTop: 5,
   },
   maintxt: {
-    color: AppColors.darkgreyColor
+    color: AppColors.darkgreyColor,
   },
   anothertxt: {
-    color: AppColors.darkgreyColor
-  }
+    color: AppColors.darkgreyColor,
+  },
 });
 
 export default CustomDropdown;
--- src/screens/WorkOrder/WorkOrder.tsx
+++ src/screens/WorkOrder/WorkOrder.tsx
@@ -1,90 +1,125 @@
-import { View, Text, SafeAreaView, StatusBar, StyleSheet, TouchableOpacity, FlatList, ScrollView, TouchableWithoutFeedback, Alert } from 'react-native'
-import React, { useState } from 'react'
-import { AppColors } from '../../utils/colors'
-import { SCREEN_HEIGHT } from '../../utils/Dimensions'
-import CustomIcon from '../../components/customIcon'
-import MyText from '../../components/customtext'
-import { ShadowStyle } from '../../utils/constants'
-import { useDeleteWorkOrderMutation, useGetAllWorkOrderQuery } from '../../services/RTKClient'
-import Loader from '../../components/Loader'
-import { useFocusEffect } from '@react-navigation/native'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import { AppColors } from "../../utils/colors";
+import { SCREEN_HEIGHT } from "../../utils/Dimensions";
+import CustomIcon from "../../components/customIcon";
+import MyText from "../../components/customtext";
+import { ShadowStyle } from "../../utils/constants";
+import {
+  useDeleteWorkOrderMutation,
+  useGetAllWorkOrderQuery,
+} from "../../services/RTKClient";
+import Loader from "../../components/Loader";
+import { useFocusEffect } from "@react-navigation/native";
 
 export default function WorkOrder({ navigation }) {
-  const { data: workOrder, isLoading: isLoading1, refetch } = useGetAllWorkOrderQuery()
+  const {
+    data: workOrder,
+    isLoading: isLoading1,
+    refetch,
+  } = useGetAllWorkOrderQuery();
   const [show, setShow] = useState(false);
-  const [selected, setSelected] = useState({})
-  const [deleteWorkOrder, {isLoading: isLoading2}] = useDeleteWorkOrderMutation()
+  const [selected, setSelected] = useState({});
+  const [deleteWorkOrder, { isLoading: isLoading2 }] =
+    useDeleteWorkOrderMutation();
 
   const showAlert = () => {
     Alert.alert(
-      'Delete',
-      'Are you confirm to delete this work order?', // <- this part is optional, you can pass an empty string
+      "Delete",
+      "Are you confirm to delete this work order?", // <- this part is optional, you can pass an empty string
       [
-        {text: 'DELETE', onPress: () => onDelete()},
-        {text: 'Cancel', onPress: () => setShow(false)},
+        { text: "DELETE", onPress: () => onDelete() },
+        { text: "Cancel", onPress: () => setShow(false) },
       ],
-      {cancelable: false},
+      { cancelable: false }
     );
-  }
+  };
 
-  const onDelete =() => {
+  const onDelete = () => {
     deleteWorkOrder(selected)
-    .unwrap()
-    .then((payload) => {
-      setShow(false)
-      refetch()
-      setShow(false)
-    })
-    .catch((error) => {
-      console.log("Error", error);
-      setShow(false)
-    })
-  } 
+      .unwrap()
+      .then((payload) => {
+        setShow(false);
+        refetch();
+        setShow(false);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+        setShow(false);
+      });
+  };
 
   useFocusEffect(
     React.useCallback(() => {
-      refetch()
+      refetch();
     }, [])
-   );
+  );
 
-  
   return (
-
-
     <SafeAreaView style={{ backgroundColor: AppColors.white, flex: 1 }}>
-      <StatusBar backgroundColor={AppColors.white} barStyle={'dark-content'} translucent={false} />
-  <Loader loading={isLoading1 || isLoading2}/>
+      <StatusBar
+        backgroundColor={AppColors.white}
+        barStyle={"dark-content"}
+        translucent={false}
+      />
+      <Loader loading={isLoading1 || isLoading2} />
       <ScrollView>
-        <TouchableWithoutFeedback style={{ marginTop: StatusBar.currentHeight }} onPress={() => {
-          setSelected({})
-          setShow(false)
-        }}>
+        <TouchableWithoutFeedback
+          style={{ marginTop: StatusBar.currentHeight }}
+          onPress={() => {
+            setSelected({});
+            setShow(false);
+          }}
+        >
           <View style={{ padding: 15 }}>
             <View style={styles.mainRow}>
-              <View >
-                <MyText fontType="bold" style={{
-                  fontSize: 18,
-                  color: AppColors.black,
-                }} >Work Order</MyText>
+              <View>
+                <MyText
+                  fontType="bold"
+                  style={{
+                    fontSize: 18,
+                    color: AppColors.black,
+                  }}
+                >
+                  Work Order
+                </MyText>
               </View>
               <View style={styles.AC}>
-                <CustomIcon name='notifications-outline' />
+                <CustomIcon name="notifications-outline" />
               </View>
             </View>
 
             <TouchableOpacity style={styles.filter}>
-              <MyText>
-                Filters
-              </MyText>
-              <CustomIcon name='chevron-forward' color={AppColors.darkgreyColor} />
+              <MyText>Filters</MyText>
+              <CustomIcon
+                name="chevron-forward"
+                color={AppColors.darkgreyColor}
+              />
             </TouchableOpacity>
             <FlatList
               data={workOrder?.workOrder}
               showsHorizontalScrollIndicator={false}
               scrollEnabled={false}
-              renderItem={({ item }) =>
+              renderItem={({ item }) => (
                 <View style={[styles.card, ShadowStyle]}>
-                  <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      width: "100%",
+                    }}
+                  >
                     <View>
                       <MyText fontType="medium" style={{ fontSize: 14 }}>
                         Work Order Code
@@ -93,13 +128,23 @@ export default function WorkOrder({ navigation }) {
                         {item.work_order_type}
                       </MyText>
                     </View>
-                    <CustomIcon name='ellipsis-vertical' onPress={() => {
-                      setSelected(item)
-                      setShow(true)
-                    }} />
+                    <CustomIcon
+                      name="ellipsis-vertical"
+                      onPress={() => {
+                        setSelected(item);
+                        setShow(true);
+                      }}
+                    />
                   </View>
 
-                  <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '100%', marginTop: 30 }}>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      width: "100%",
+                      marginTop: 30,
+                    }}
+                  >
                     <View>
                       <MyText fontType="medium" style={{ fontSize: 14 }}>
                         Client name
@@ -118,33 +163,54 @@ export default function WorkOrder({ navigation }) {
                     </View>
                   </View>
                 </View>
-              }
+              )}
             />
-
           </View>
         </TouchableWithoutFeedback>
       </ScrollView>
-      {
-        show && <View style={[{ backgroundColor: AppColors.white, paddingVertical: 30, margin: 10, borderRadius: 10, alignItems: 'center' }, ShadowStyle]}>
-          <MyText style={{ fontSize: 16, margin: 10 }} fontType="medium"
-          onPress={() =>{ navigation.navigate("ViewWorkOrder",{
-            OrderId: selected?.work_order_id
-          }), setShow(false)}} 
+      {show && (
+        <View
+          style={[
+            {
+              backgroundColor: AppColors.white,
+              paddingVertical: 30,
+              margin: 10,
+              borderRadius: 10,
+              alignItems: "center",
+            },
+            ShadowStyle,
+          ]}
+        >
+          <MyText
+            style={{ fontSize: 16, margin: 10 }}
+            fontType="medium"
+            onPress={() => {
+              navigation.navigate("ViewWorkOrder", {
+                OrderId: selected?.work_order_id,
+              }),
+                setShow(false);
+            }}
           >
             Edit record
           </MyText>
-          <MyText style={{ fontSize: 16, margin: 10 }} fontType="medium" onPress={()=> showAlert()}>
+          <MyText
+            style={{ fontSize: 16, margin: 10 }}
+            fontType="medium"
+            onPress={() => showAlert()}
+          >
             Delete record
           </MyText>
         </View>
-      }
+      )}
 
-      <TouchableOpacity style={[styles.fab, ShadowStyle]} onPress={() => navigation.navigate("GenerateTicket")}>
-        <CustomIcon name='add' color={AppColors.white} size={30} />
+      <TouchableOpacity
+        style={[styles.fab, ShadowStyle]}
+        onPress={() => navigation.navigate("GenerateTicket")}
+      >
+        <CustomIcon name="add" color={AppColors.white} size={30} />
       </TouchableOpacity>
     </SafeAreaView>
-
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -152,45 +218,44 @@ const styles = StyleSheet.create({
     backgroundColor: AppColors.primary,
     borderBottomLeftRadius: 50,
     borderBottomRightRadius: 50,
-    paddingBottom: 30
+    paddingBottom: 30,
   },
   mainRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   AC: {
-
     backgroundColor: AppColors.lightgrey,
     borderRadius: 50,
-    height: 40, width: 40,
-    alignItems: 'center',
-    justifyContent: 'center'
+    height: 40,
+    width: 40,
+    alignItems: "center",
+    justifyContent: "center",
   },
   filter: {
-    justifyContent: 'space-between',
-    flexDirection: 'row',
+    justifyContent: "space-between",
+    flexDirection: "row",
     borderRadius: 5,
     padding: 10,
     backgroundColor: AppColors.darkgrey,
-    marginTop: 10
+    marginTop: 10,
   },
   card: {
     backgroundColor: AppColors.white,
-    alignItems: 'flex-start',
+    alignItems: "flex-start",
     borderRadius: 10,
     padding: 20,
-    margin: 10
-
+    margin: 10,
   },
   fab: {
     backgroundColor: AppColors.primary,
     borderRadius: 50,
     width: 50,
     height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
     bottom: 50,
-    right: 20
-  }
-})
+    right: 20,
+  },
+});
